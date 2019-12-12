e4988e9311b6b1314804c6612cc1e4d1c0e74174
= <=>
2019-12-12 08:03:34 +0100

    Botón Share terminado

--- app/src/main/java/com/miguelmartin/tuconsumo/model/ResultadoModel.kt
+++ app/src/main/java/com/miguelmartin/tuconsumo/model/ResultadoModel.kt
@@ -1,5 +1,9 @@
 package com.miguelmartin.tuconsumo.model
 
+import android.content.Context
+import com.miguelmartin.tuconsumo.Entities.Resultados
+import com.miguelmartin.tuconsumo.R
+
 class ResultadoModel {
 
     fun sumarRestarPasajeros(oldPasajeros: Int, cambio: Int): Int {
@@ -9,4 +13,9 @@ class ResultadoModel {
 
     fun calcularCostoPorPasajero(costo: Double, pasajeros: Int): Double = costo/pasajeros
 
+    fun generarTextoParaCompartir(resultados: Resultados,numPasajeros: Int, nombreApp:String) =
+        "Se han recorrido ${resultados.distancia} Km.\n" +
+        "El consumo ha costado ${resultados.costo}€.\n" +
+        "Entre $numPasajeros passajeros, ${calcularCostoPorPasajero(resultados.costo, numPasajeros)}€ cada uno.\n" +
+        "***$nombreApp***"
 }
--- app/src/main/java/com/miguelmartin/tuconsumo/presenter/ResultadoPresenter.kt
+++ app/src/main/java/com/miguelmartin/tuconsumo/presenter/ResultadoPresenter.kt
@@ -1,5 +1,9 @@
 package com.miguelmartin.tuconsumo.presenter
 
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import com.miguelmartin.tuconsumo.Entities.Resultados
+import com.miguelmartin.tuconsumo.R
 import com.miguelmartin.tuconsumo.model.ResultadoModel
 import com.miguelmartin.tuconsumo.view.ResultadoActivity
 
@@ -12,6 +16,20 @@ class ResultadoPresenter(resultadoActivity: ResultadoActivity) {
         resultadoActivity.setPasajeros(newPasajeros)
         resultadoActivity.visibilityPagoPorPasajero(newPasajeros)
         resultadoActivity.setPagoPorPasajero(resultadoModel.calcularCostoPorPasajero(costo, newPasajeros))
+    }
+
+    fun compartirReparto(resultados: Resultados, numPasajeros: Int) {
+        val textoParaCompartir = resultadoModel.generarTextoParaCompartir(resultados, numPasajeros, resultadoActivity.getString(R.string.app_name))
+
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, textoParaCompartir)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        resultadoActivity.startActivity(shareIntent)
+
     }
 
 
