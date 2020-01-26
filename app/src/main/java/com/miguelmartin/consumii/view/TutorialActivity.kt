4ea38ef7201c84cc8493be41a66b50ee5d83bac7
= <=>
2020-01-26 11:25:16 +0100

    Release v3.1, bug idioma de la guía

--- app/src/main/java/com/miguelmartin/consumii/view/TutorialActivity.kt
+++ app/src/main/java/com/miguelmartin/consumii/view/TutorialActivity.kt
@@ -3,10 +3,11 @@ package com.miguelmartin.consumii.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import com.miguelmartin.consumii.Common.BaseActivity
 import com.miguelmartin.consumii.R
 import kotlinx.android.synthetic.main.settings_activity.*
 
-class TutorialActivity : AppCompatActivity() {
+class TutorialActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
